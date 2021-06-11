d93422b4cc3b6035ef72ce5a3dfb0e8e3be932a1
crims0n <crims0n@ya.ru>
2021-06-11 17:28:36 +0300

    xfce4

--- docker/02-build.sh
+++ docker/02-build.sh
@@ -2,5 +2,5 @@
 #docker run -d --name mlc --privileged -v /build:/build local/mlc /build/slax/autoinstall -
 #docker run --rm -it --privileged -v /build:/build local/mlc /build/slax/autoinstall -
 #docker run --rm -it --privileged -v /build:/build local/mlc /build/slax/autoinstall build_modules_chroot -
-docker run -it --name mlc --privileged -v /build:/build local/mlc
+docker run -it --name mlc --privileged -v /build:/build local/mlc /build/minios-xfce/autoinstall build_modules -
 #docker run -d --name mlc --privileged -v /build:/build local/mlc
