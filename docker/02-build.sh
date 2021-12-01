cb115b0133be03eea3048352adadd186bfbcf0c1
crims0n <crims0n@ya.ru>
2021-12-01 11:53:20 +0300

    Update

--- docker/02-build.sh
+++ docker/02-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#docker run -d --name mlc --privileged -v /build:/build local/mlc
-docker run -d --name mlc-modules --privileged -v /build:/build local/mlc /build/minios-slax/install build_modules -
+docker run -d --name mlc --privileged -v /build:/build local/mlc
+#docker run -d --name mlc-modules --privileged -v /build:/build local/mlc /build/minios-slax/install build_modules -
--- docker/Dockerfile.py
+++ docker/Dockerfile.py
@@ -12,8 +12,8 @@ class DockerFile(pydocker.DockerFile):
     """   add here your custom features   """
 
 #d = DockerFile(base_img='ubuntu:focal', name='local/mlc:latest')
-#d = DockerFile(base_img='ubuntu:bionic', name='local/mlc:latest')
-d = DockerFile(base_img='debian:buster', name='local/mlc:latest')
+d = DockerFile(base_img='ubuntu:bionic', name='local/mlc:latest')
+#d = DockerFile(base_img='debian:buster', name='local/mlc:latest')
 
 d.RUN_bash_script('/opt/install.sh', r'''
 touch /.minios-live-container
