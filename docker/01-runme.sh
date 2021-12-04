bc90ce57d74aa6cbead841c2d54558803efef177
crim50n <crims0n@ya.ru>
2021-12-05 00:19:07 +0300

    fix for the stretch slax build

--- docker/01-runme.sh
+++ docker/01-runme.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Several packages need to be installed to use Dockerfile.py
 # It creates a local container that you can use to build minios-live
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 apt update && apt install -y docker.io python3-pip
 systemctl start docker
 systemctl enable docker
 pip3 install pydocker
-python3 ./Dockerfile.py
+python3 $SCRIPT_DIR/Dockerfile.py
