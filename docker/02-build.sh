05d8f3b696fd5783d6d43219ac44015457f7d4ee
crim50n <crims0n@ya.ru>
2021-12-15 01:37:10 +0300

    now using the latest version of Ubuntu to build

--- docker/02-build.sh
+++ docker/02-build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker run -d --name mlc --privileged -v /build:/build local/mlc /build/minios-live/install -
+#docker run -d --name mlc --privileged -v /build:/build local/mlc-rolling /build/minios-live/install -
