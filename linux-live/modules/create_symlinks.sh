4639d25aed9f548d8c4518593c70efef18c66a7b
crim50n <crims0n@ya.ru>
2022-02-19 18:29:58 +0300

    fix

--- linux-live/modules/create_symlinks.sh
+++ linux-live/modules/create_symlinks.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 mkdir $1
 for file in $1/01-firmware \
-            $1/02-xorg \
-            $1/03-xfce-desktop \
-            $1/04-xfce-apps \
-            $1/05-chromium
-do
-   ln -s ../../scripts/$(basename $file) $file
+    $1/02-xorg \
+    $1/03-xfce-desktop \
+    $1/04-xfce-apps \
+    $1/05-chromium \
+    $1/06-virtualbox; do
+    if [ -f $file ]; then
+        rm $file
+    fi
+    ln -s ../../scripts/$(basename $file) $file
 done
