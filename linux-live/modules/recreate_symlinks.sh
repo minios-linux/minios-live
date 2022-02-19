2cad747f8bf7dfac6f8dca24ef29d0c6952bf4f4
crim50n <crims0n@ya.ru>
2022-02-19 18:35:09 +0300

    fix

--- linux-live/modules/recreate_symlinks.sh
+++ linux-live/modules/recreate_symlinks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-for file in $1/*
-do
-    rm $file
-    ln -s ../../scripts/$(basename $file) $file
+for file in $1/*; do
+   rm $file
+   ln -s ../../scripts/$(basename $file) $file
 done
