7192b44947aea64563ddcc3a30dcfda0bdb94c70
crim50n <crims0n@yandex.ru>
2023-09-01 11:07:46 +0300

    Docker scripts update

--- docker/01-runme.sh
+++ docker/01-runme.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Several packages need to be installed to use Dockerfile.py
 # It creates a local container that you can use to build minios-live
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-apt update && apt install -y docker.io python3-pip
+apt update && apt install -y docker.io
 systemctl start docker
 systemctl enable docker
-pip3 install pydocker
-python3 $SCRIPT_DIR/Dockerfile.py
+docker build . -t local/mlc:latest
--- docker/02-build.sh
+++ docker/02-build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 docker run -d --name mlc --privileged --device-cgroup-rule='b 7:* rmw' -v /dev:/dev -v /build:/build local/mlc /build/minios-live/install -
-#docker run -d --name mlc --privileged -v /build:/build local/mlc-rolling /build/minios-live/install -
