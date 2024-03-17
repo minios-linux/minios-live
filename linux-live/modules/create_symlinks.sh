684d2015ebc5ba9de86dd67438f7acfcdc2d7dea
crim50n <crims0n@yandex.ru>
2024-03-18 02:28:15 +0300

    Update 20240318

--- linux-live/modules/create_symlinks.sh
+++ linux-live/modules/create_symlinks.sh
@@ -7,7 +7,10 @@ for file in $1/00-minios \
    $1/01-kernel \
    $1/02-firmware \
    $1/03-xorg \
-   $1/04-mediaplayer; do
+   $1/04-xfce-desktop \
+   $1/05-xfce-apps \
+   $1/06-software \
+   $1/07-firefox; do
    if [ -L $file ]; then
       rm $file
    fi
