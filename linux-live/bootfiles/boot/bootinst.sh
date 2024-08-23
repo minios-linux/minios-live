e37d099b83b3c580cb9cd19f8837c40463a3c39a
h-v-smacker <hans-von-smacker+github@gmail.com>
2024-08-23 20:12:38 +0300

    Make the script run from fat32 without executable flags and some eyecandy messages

--- linux-live/bootfiles/boot/bootinst.sh
+++ linux-live/bootfiles/boot/bootinst.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Setup booting from disk (USB or harddrive)
 # Requires: fdisk, df, tail, tr, cut, dd, sed
+# If this script is placed on a fat32 (vfat) partition, 
+#                   run it with `sudo bash bootinst.sh`
 
 # change working directory to dir from which we are started
 CWD="$(pwd)"
@@ -12,10 +14,34 @@ PART="$(df . | tail -n 1 | tr -s " " | cut -d " " -f 1)"
 DEV="$(echo "$PART" | sed -r "s:[0-9]+\$::" | sed -r "s:([0-9])[a-z]+\$:\\1:i")"   #"
 
 ARCH=$(uname -m)
-if [ "$ARCH" = "x86_64" ]; then ARCH=64; else ARCH=32; fi
-EXTLINUX=extlinux.x$ARCH
 
-./"$EXTLINUX" --install "$BOOT"
+if ! [ -w "$DEV" ]; then
+    echo "You have no permissions to write to ${DEV}."
+    echo "You should run this script with sudo."
+    exit 1
+fi
+
+echo "MiniOS will be installed to:"
+
+echo "    Target device:   " $DEV
+echo "    Target partition:" $PART
+echo "    CPU architecture:" $ARCH
+
+echo "If this is what you want, press ENTER to continue."
+echo "If this is incorrect, press Ctrl+C to abort."
+read r
+
+echo "Beginning installation..."
+
+if [ "$ARCH" = "x86_64" ]; then 
+    ARCH=64; 
+    EXTLINUX=extlinux.x$ARCH
+    /lib64/ld-linux-x86-64.so.2 ./"$EXTLINUX" --install "$BOOT"
+else 
+    ARCH=32; 
+    EXTLINUX=extlinux.x$ARCH
+    /lib/ld-linux.so.2 ./"$EXTLINUX" --install "$BOOT"
+fi
 
 if [ $? -ne 0 ]; then
    echo "Error installing boot loader."
