36717b67ac77e5f38933591b661d6afbfc663931
crim50n <crims0n@yandex.ru>
2024-11-15 16:59:59 +0300

    Update 20241115

--- docker/01-runme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# It creates a local container that you can use to build minios-live
-
-apt update && apt install -y docker.io
-systemctl start docker
-systemctl enable docker
-docker build . -t local/mlc:latest
--- docker/02-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -d --name mlc --privileged --device-cgroup-rule='b 7:* rmw' -v /dev:/dev -v /build:/build local/mlc /build/minios-live/install -
