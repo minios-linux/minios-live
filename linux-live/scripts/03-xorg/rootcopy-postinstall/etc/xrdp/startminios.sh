9f620be89e55316cd490a373aba7635d10a2d672
crim50n <crims0n@yandex.ru>
2025-03-17 14:51:41 +0300

    Replaced the install_packages function with the install_packages script, which has much more capabilities. Reorganized the structure of base modules scripts.

--- linux-live/scripts/03-xorg/rootcopy-postinstall/etc/xrdp/startminios.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export XDG_CURRENT_DESKTOP=XFCE
-exec /etc/xrdp/startwm.sh
--- linux-live/scripts/05-xfce-apps/rootcopy-install/usr/sbin/clonezilla.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/clonezilla -s
-/usr/sbin/ocs-live-final-action
-
-#clonezilla leaves these mounted when the program is finished. These need to be umounted.
-umount /home/partimag &> /dev/null
-umount /tmp/local-dev &> /dev/null
