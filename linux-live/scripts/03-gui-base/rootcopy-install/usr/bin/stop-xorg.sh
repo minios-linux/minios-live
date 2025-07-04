719282ab203ee2c242d24a03ad1dde4e531a9615
crims0n <crims0n@yandex.ru>
2025-07-04 22:27:21 +0300

    Add xserver-xorg-legacy package and update Xwrapper.config; enhance start/stop scripts to extract username from command line

--- linux-live/scripts/03-gui-base/rootcopy-install/usr/bin/stop-xorg.sh
+++ linux-live/scripts/03-gui-base/rootcopy-install/usr/bin/stop-xorg.sh
@@ -9,6 +9,18 @@ for _FILE in /etc/live/config.conf /etc/live/config.conf.d/*.conf; do
 done
 set +o allexport
 
+# Append command line parameters to the LIVE_CONFIG_CMDLINE variable
+LIVE_CONFIG_CMDLINE="${LIVE_CONFIG_CMDLINE:+$LIVE_CONFIG_CMDLINE }$(cat /proc/cmdline)"
+
+# Extract the username from the command line parameters
+for _PARAMETER in ${LIVE_CONFIG_CMDLINE}; do
+    case "${_PARAMETER}" in
+    live-config.username=* | username=*)
+        LIVE_USERNAME="${_PARAMETER#*username=}"
+        ;;
+    esac
+done
+
 # First, try to gracefully terminate the X session
 pkill -TERM -u "$LIVE_USERNAME" Xorg
 sleep 1
