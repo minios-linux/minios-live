cce269aff8a9807b6a01ba427b53f22742c52116
crim50n <crims0n@yandex.ru>
2025-10-11 18:07:49 +0300

    Refactor MiniOS shutdown script for improved clarity and functionality

--- linux-live/initramfs/dracut-mos/90minios/minios-shutdown.sh
+++ linux-live/initramfs/dracut-mos/90minios/minios-shutdown.sh
@@ -1,104 +1,87 @@
 #!/bin/sh
-# MiniOS shutdown hook for dracut. It's automatically started by
-# systemd (if you use it) on shutdown, no need for any tweaks.
+# Shutdown script for initramfs. It's automatically started by
+# dracut's shutdown hook when system is powering off/rebooting.
 # Purpose of this script is to unmount everything cleanly.
 #
 # Author: Tomas M <http://www.linux-live.org/>
 # Author: crims0n <crims0n@minios.dev>
 
-# Prevent multiple runs - this script should only run once
-if [ -f /run/minios-shutdown-done ]; then
-    exit 0
+# ANSI color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+RED='\033[0;31m'
+WHITE='\033[1;37m'
+RESET='\033[0m'
+
+# Detect shutdown type from kernel command line or environment
+SHUTDOWN_TYPE="shutdown"
+if grep -q "reboot" /proc/cmdline 2>/dev/null || [ "$1" = "reboot" ]; then
+   SHUTDOWN_TYPE="reboot"
+elif cat /proc/1/cmdline 2>/dev/null | grep -q "reboot"; then
+   SHUTDOWN_TYPE="reboot"
 fi
 
-LIVEKITNAME="minios"
-BEXT="sb"
-
-# Dracut provides its own logging functions
-type info >/dev/null 2>&1 || . /lib/dracut-lib.sh
-
-info "Entering MiniOS shutdown procedures"
-
 detach_free_loops() {
-    losetup -a | cut -d : -f 1 | xargs -r -n 1 losetup -d
+   losetup -a | cut -d : -f 1 | xargs -r -n 1 losetup -d
 }
 
 # $1=dir
 umount_all() {
-    tac /proc/mounts | cut -d " " -f 2 | grep ^$1 | while read LINE; do
-        umount $LINE 2>/dev/null || umount -l $LINE 2>/dev/null
-        detach_free_loops
-    done
+   tac /proc/mounts | cut -d " " -f 2 | grep "^$1" | while read LINE; do
+      umount "$LINE" 2>/dev/null
+      detach_free_loops
+   done
 }
 
-# Update devs so we are aware of all active /dev/loop* files.
-# Detach loop devices which are no longer used
-info "Detaching loops"
+echo -e "${WHITE}[${GREEN}*${WHITE}]${RESET} Detaching loop devices..."
+if command -v mdev >/dev/null 2>&1; then
+    mdev -s 2>/dev/null || true
+fi
 detach_free_loops
 
-# do it the dirty way, simply try to umount everything to get rid of most mounts
-info "Unmounting union"
-
-# First, try to kill any processes that might be using these mounts
-if command -v fuser >/dev/null 2>&1; then
-    fuser -km /oldroot 2>/dev/null || true
-    sleep 1
-fi
-
+echo -e "${WHITE}[${GREEN}*${WHITE}]${RESET} Unmounting union filesystem..."
 umount_all /oldroot
 
-# If oldroot is still mounted, there is something blocking.
-# Lets move it somewhere so we can unmount it cleanly
-info "Moving mounts outside of union if necessary"
+# Remember from which device we are started, so we can eject it later
+DEVICE="$(cat /proc/mounts | grep -E '/(memory|initramfs/memory)/data' | grep /dev/ | head -n1 | cut -d " " -f 1)"
+
+echo -e "${WHITE}[${GREEN}*${WHITE}]${RESET} Relocating blocking mounts..."
 NR=100
-tac /proc/mounts | cut -d " " -f 2 | grep ^/oldroot/. | while read LINE; do
-    NR=$(($NR + 1))
-    mkdir -p /move/$NR
-    mount --move $LINE /move/$NR 2>/dev/null
-    umount /oldroot 2>/dev/null
+tac /proc/mounts | cut -d " " -f 2 | grep "^/oldroot/" | while read LINE; do
+   NR=$((NR + 1))
+   mkdir -p /move/$NR
+   mount --move "$LINE" /move/$NR 2>/dev/null
+   umount /oldroot 2>/dev/null
 done
 
-# remember from which device we are started, so we can eject it later
-# In shutdown initramfs, the old root is at /oldroot
-DEVICE="$(cat /proc/mounts | grep -E '(^/oldroot)?/lib/live/mount/medium' | grep /dev/ | cut -d " " -f 1 | head -1)"
-
-info "going through several cycles of umounts to clear everything left"
+echo -e "${WHITE}[${GREEN}*${WHITE}]${RESET} Clearing remaining mounts..."
 for i in 1 2 3 4; do
-    for d in $(ls -1 /move 2>/dev/null | sort); do
-        umount_all /move/$d
-    done
+   for d in $(ls -1 /move 2>/dev/null | sort); do
+      umount_all /move/$d
+   done
 done
 
-# Try to unmount both /lib/live/mount and /oldroot/lib/live/mount
-# depending on whether we're in shutdown initramfs or not
-umount_all /oldroot/lib/live/mount
-umount_all /lib/live/mount
+echo -e "${WHITE}[${GREEN}*${WHITE}]${RESET} Unmounting memory filesystem..."
+umount_all /oldroot/run/initramfs/memory
+umount_all /oldsys/run/initramfs/memory
+umount_all /run/initramfs/memory
+umount_all /memory
 
-# Force unmount any remaining live mount points
-for mp in /oldroot/lib/live/mount/medium /lib/live/mount/medium \
-    /oldroot/lib/live/mount/changes /lib/live/mount/changes \
-    /oldroot/lib/live/mount/bundles /lib/live/mount/bundles; do
-    if grep -q " $mp " /proc/mounts 2>/dev/null; then
-        umount -f $mp 2>/dev/null || umount -l $mp 2>/dev/null || true
-    fi
+# Eject CD/DVD if booted from optical media
+for i in $(cat /proc/sys/dev/cdrom/info 2>/dev/null | grep "^drive name:" | awk '{print $3}'); do
+   if [ "$DEVICE" = "/dev/$i" ]; then
+      echo -e "${WHITE}[${YELLOW}!${WHITE}]${RESET} Ejecting optical drive ${CYAN}/dev/$i${RESET}..."
+      eject -r /dev/$i 2>/dev/null || eject /dev/$i 2>/dev/null || true
+      echo -e "${WHITE}[${YELLOW}!${WHITE}]${RESET} CD/DVD tray will close in 6 seconds..."
+      sleep 6
+      eject -t /dev/$i 2>/dev/null || true
+   fi
 done
 
-# eject cdrom device if we were running from it
-if [ -n "$DEVICE" ]; then
-    for i in $(cat /proc/sys/dev/cdrom/info 2>/dev/null | grep name); do
-        if [ "$DEVICE" = "/dev/$i" ]; then
-            info "Attempting to eject /dev/$i..."
-            eject /dev/$i
-            info "CD/DVD tray will close in 6 seconds..."
-            sleep 6
-            eject -t /dev/$i
-        fi
-    done
+if [ "$SHUTDOWN_TYPE" = "reboot" ]; then
+   echo -e "${WHITE}[${GREEN}OK${WHITE}]${RESET} System prepared for reboot"
+else
+   echo -e "${WHITE}[${GREEN}OK${WHITE}]${RESET} System prepared for shutdown"
 fi
-
-info "Shutdown cleanup complete"
-
-# Mark that we've completed our work
-touch /run/minios-shutdown-done
-
-exit 0
